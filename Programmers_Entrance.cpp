7bfaf14127e5112198afd7b73f8701d21b2886d6
mimwin <kms105903@naver.com>
2022-07-22 19:22:53 +0900

    Programmers_입국 심사

---
+++ Programmers_Entrance.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long long solution(int n, vector<int> times) { 
+	sort(times.begin(), times.end());
+
+    long long answer = 0;
+    long long start = 1;
+    long long end = (long long) times[times.size()-1] * n;
+    long long mid;
+
+    while(start <= end) {
+
+        mid = (start + end) / 2;
+        long long cnt = 0;
+
+        for(int i = 0; i < times.size(); i++) {
+            cnt += mid / times[i];
+        }
+        if(cnt < n) {
+            start = mid + 1;
+        }
+        else {    
+            if(mid <= end) {
+                answer = mid;
+            }
+            end = mid - 1;
+        }
+    }
+    return answer;
+}
